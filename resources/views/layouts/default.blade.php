f50fd225b13fc5d9c3ae9828afa9550047c520e2
haveahill <lp_czy@163.com>
2018-01-03 03:10:03 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Simsys')</title>
+    <title>@yield('title', 'Sim system') - 模拟室管理系统</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+
+      </div>
+    </div>
+    @include('layouts._footer')
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>home</h1>
+  <div class="jumbotron">
+    <h1>Hello Simsys</h1>
+    <p class="lead">
+      你现在所看到的是 Sim System 的示例项目主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
